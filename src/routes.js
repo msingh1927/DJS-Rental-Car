c9e15314781e737e844e41e5579e13ba8645092d
edwulit <ejdwulit@syr.edu>
2023-03-19 21:23:30 -0400

    Working main car listing page with filter

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,4 @@
 import Home from "./components/HomeView.vue";
-import Registration from "./components/RegistrationView.vue";
-import BookedCars from "./components/BookedCars.vue";
-import carService from "./components/carService.vue";
 import CarsListView from "./components/CarsListView.vue"
 import AdminPage from "./components/AdminPage.vue"
 import { createRouter, createWebHistory } from "vue-router";
@@ -14,18 +11,7 @@ const routes = [
     component: Home,
   },
   {
-    path: "/Registration",
-    name: "Registration",
-    component: Registration,
-  },
-  ,
-  {
-    path: "/carService",
-    name: "carService",
-    component: carService,
-  },
-  {
-    path: "/carlistview",
+    path: "/carlist",
     name: "CarsListView",
     component: CarsListView,
   },
@@ -33,12 +19,7 @@ const routes = [
     path: "/admin",
     name: "AdminPage",
     component: AdminPage,
-  },
-  {
-    path: "/BookedCars",
-    name: "BookedCars",
-    component: BookedCars,
-  },
+  }
 ];
 
 const router = createRouter({ history: createWebHistory(), routes });
